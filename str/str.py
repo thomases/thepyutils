d786aa6b0d76b5af9e4e8e49e9ecaaec79a9a866
Thomas Espe <thomas.espe@gmail.com>
2015-07-01 13:32:04 +0200

    Added reST to the docstring of all_but_last

--- str/str.py
+++ str/str.py
@@ -6,5 +6,12 @@
 
 
 def all_but_last(s, sep=' '):
-    """Returns all but the last element of a string after split on sep"""
+    """
+    Returns all but the last element of a string after split on sep
+
+    :param s str: The string to operate on
+    :param sep str: The separator to use for split, default ' '
+    :return: A string with all but the last element of the input string
+    :rtype: string
+    """
     return sep.join(s.split(sep)[0:-1])
